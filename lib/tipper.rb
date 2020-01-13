66087bf77786144c863bc573df4153ceec140cae
Olivia McGoffin <olivia@thoughtbot.com>
2020-01-13 10:58:39 -0800

    My solution for replacing vars with methods

--- lib/tipper.rb
+++ lib/tipper.rb
@@ -8,11 +8,19 @@ class Tipper
   end
 
   def total
-    tax_amount = amount * TAX
-    discount_amount = amount * (discount_percentage / 100.0)
-    tip_amount = amount * (tip_percentage / 100.0)
-
     amount + tax_amount - discount_amount + tip_amount
+  end
+
+  def tax_amount
+    amount * TAX
+  end
+
+  def discount_amount
+    amount * (discount_percentage / 100.0)
+  end
+
+  def tip_amount
+    amount * (tip_percentage / 100.0)
   end
 
   private
